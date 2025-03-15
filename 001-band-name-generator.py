bc4edde189f6125c4183a1b8d50064dd3bd5d71e
jsaikumar76 <jsaikumar76@outlook.com>
2025-03-15 23:34:52 +0530

    day_001: created a band name using print and input functions.

---
+++ 001-band-name-generator.py
@@ -0,0 +1,7 @@
+print("Welcome to band name generator.")
+print("Where did you grow up? ")
+first_name = input()
+print("What is your pet name? ")
+last_name = input()
+band_name = first_name + " " + last_name
+print("your band name could be " + band_name)
